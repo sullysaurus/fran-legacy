55c47f249ea5ede4c3e3b91b7fa6189ee22b7a38
Danny Sullivan <dsully15@gmail.com>
2026-08-20 12:46:56 -0400

    Use Netlify URLs for social previews

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://franlegacy.com',
+  site: 'https://franlegacy.netlify.app',
   integrations: [sitemap()],
 });
